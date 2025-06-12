245bf812f4ab7dfca50413c47458ab49f3ef8112
anjaliofficial <bistaanjali1415@gmail.com>
2025-06-13 00:45:21 +0545

    Take conditional input from user in python

--- Day1.py
+++ Day1.py
@@ -26,3 +26,16 @@ print("Total number of students:", x)
 print("Number of boys is:", y)
 print("Number of girls is:", z)
 
+#Take  conditional input from user in python 
+
+age_input = input(" Enter your age ")
+age = int (age_input)
+
+if age <0:
+    print("Please Enter a valid age ")
+elif age<18:
+    print( " Ypur are a minor ")
+elif age >= 18 and age<65:
+    print("You are an adult")
+else: 
+    print("you are a senior citizen")
