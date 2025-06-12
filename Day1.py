62b11944db0eba047ee80b4a988aca5311865f42
anjaliofficial <bistaanjali1415@gmail.com>
2025-06-13 01:00:16 +0545

    Print Numbers

--- Day1.py
+++ Day1.py
@@ -1,49 +1,57 @@
-# Simple greeting
-print("Hello, World!")
+# # 
+# # Simple greeting
+# print("Hello, World!")
 
-# Asking user for their name
-name = input("Enter your name: ")
-print("Hello,", name, "Welcome!")
+# # Asking user for their name
+# name = input("Enter your name: ")
+# print("Hello,", name, "Welcome!")
 
-# Single variable assignment
-s = "Bob"
-print(s)
+# # Single variable assignment
+# s = "Bob"
+# print(s)
 
-# Multiple variables
-S = "Alice"
-age = 25
-city = "Kathmandu"
-print(S, age, city)
+# # Multiple variables
+# S = "Alice"
+# age = 25
+# city = "Kathmandu"
+# print(S, age, city)
 
-# Taking two inputs at once
-x, y = input("Enter two values (e.g., boys girls): ").split()
-print("Number of boys:", x)
-print("Number of girls:", y)
+# # Taking two inputs at once
+# x, y = input("Enter two values (e.g., boys girls): ").split()
+# print("Number of boys:", x)
+# print("Number of girls:", y)
 
-# Taking three inputs at once
-x, y, z = input("Enter three values (e.g., total boys girls): ").split()
-print("Total number of students:", x)
-print("Number of boys is:", y)
-print("Number of girls is:", z)
+# # Taking three inputs at once
+# x, y, z = input("Enter three values (e.g., total boys girls): ").split()
+# print("Total number of students:", x)
+# print("Number of boys is:", y)
+# print("Number of girls is:", z)
 
-#Take  conditional input from user in python 
-# Prompting the user for input
-age_input = input("Enter your age: ")
+# #Take  conditional input from user in python 
+# # Prompting the user for input
+# age_input = input("Enter your age: ")
 
-# Converting the input to an integer
-age = int(age_input)
+# # Converting the input to an integer
+# age = int(age_input)
 
-# Checking conditions based on user input
-if age < 0:
-    print("Please enter a valid age.")
-elif age < 18:
-    print("You are a minor.")
-elif age >= 18 and age < 65:
-    print("You are an adult.")
-else:
-    print("You are a senior citizen.")
+# # Checking conditions based on user input
+# if age < 0:
+#     print("Please enter a valid age.")
+# elif age < 18:
+#     print("You are a minor.")
+# elif age >= 18 and age < 65:
+#     print("You are an adult.")
+# else:
+#     print("You are a senior citizen.")
 
 
-#Print Names in Python
-color = input("What color is rose?")
-print(color)
+# #Print Names in Python
+# color = input("What color is rose?")
+# print(color)
+
+
+
+# Print Numbers in Python
+ 
+n = int(input("How many roses? "))
+print (n)       
