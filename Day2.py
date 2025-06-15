ccbdec73bed9fdab8f76c8cdc40b4d5f548d50f9
anjaliofficial <bistaanjali1415@gmail.com>
2025-06-15 18:27:10 +0545

    identity operators in python

--- Day2.py
+++ Day2.py
@@ -241,3 +241,16 @@ print(b)
 b <<= a
 print(b)
 
+
+
+# Identity Operators in Python
+
+# In Python, is and is not are the identity operators both are used to check if two values are located on the same part of the memory. Two variables that are equal do not imply that they are identical. 
+
+#example of identity operators in python 
+a = 10
+b = 20
+c = a
+
+print(a is not b)
+print(a is c)
