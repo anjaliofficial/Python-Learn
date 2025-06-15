bf47ab897f9f9fc31a1996dc9e88737f700cd23b
anjaliofficial <bistaanjali1415@gmail.com>
2025-06-15 17:54:50 +0545

    global varibale

--- Day2.py
+++ Day2.py
@@ -93,3 +93,13 @@ def f ():
 
 f()
     #print (a) thi would raise an error since "local _var" is not accessible outside the function 
+
+#global variable 
+a = "I am global  "
+def f():
+    global a
+    a = "Modified globally"
+    print(a)
+
+f()
+print(a)
