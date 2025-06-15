a0b6ae704b591a2a8f169ea31e0400f3664d5cea
anjaliofficial <bistaanjali1415@gmail.com>
2025-06-15 17:40:36 +0545

    varlid examples and invalid examples of naming variables

--- Day2.py
+++ Day2.py
@@ -2,3 +2,18 @@ x = 5
 name = "Anjali"
 print(x)
 print(name)
+
+
+
+#Rules for naminf var3iables 
+
+#valid examples 
+age = 11
+_colour = "lilac"
+total_score = 90
+
+# invalid example
+
+# 1name = "Error"  # Starts with a digit
+# class = 10       # 'class' is a reserved keyword
+# user-name = "Doe"  # Contains a hyphen
