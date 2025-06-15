ff7f08374d56e5ae66521416d801388175110eae
anjaliofficial <bistaanjali1415@gmail.com>
2025-06-15 18:16:37 +0545

    Logical operators in python

--- Day2.py
+++ Day2.py
@@ -189,3 +189,14 @@ print(a <= b)
 
 
 # logical operators in python 
+# Python Logical operators perform Logical AND, 
+# Logical OR and Logical NOT operations. 
+# It is used to combine conditional statements.
+
+# Exaple of logical operators in python 
+
+a = True
+b = False
+print(a and b)
+print(a or b)
+print(not a)
