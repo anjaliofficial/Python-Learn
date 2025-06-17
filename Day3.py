af49ff9eb4faf5a08a6c4e78acedd4f5a227256c
anjaliofficial <bistaanjali1415@gmail.com>
2025-06-18 00:22:27 +0545

    all keywords

--- Day3.py
+++ Day3.py
@@ -2,3 +2,9 @@
 # Python Keywords
 
 
+#getting list of all python keywords 
+
+import keyword
+# Printing all keywords at once using "Kwlist()"
+print("The list of keyword is : ")
+print(keyword.kwlist)
