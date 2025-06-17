47033476943fa56405a07f823c9d56bcc9c6597a
anjaliofficial <bistaanjali1415@gmail.com>
2025-06-18 00:29:23 +0545

    Values keyword True, falsem none keyword, del

--- Day3.py
+++ Day3.py
@@ -20,3 +20,20 @@ print(keyword.kwlist)
 #  Scope and Namespace-------------------	global, nonlocal
 # Async Programming----------------	async, await
 
+
+#Value Keywords: True, False, None Keyword, del
+print(False == 0)
+print(True == 1)
+
+#print(False == 0)
+print(True == 1)
+
+# True + True + True is  3
+print(True + True + True)
+
+# True + False + False is  1
+print(True + False + False)
+
+# None isn't equal to  0  or an empty list []
+print(None == 0)
+print(None == [])
