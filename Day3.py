df80047d712b4ac27f28f05aebea6dccd2c92a3b
anjaliofficial <bistaanjali1415@gmail.com>
2025-06-18 00:27:10 +0545

    All categooery and keywords in python

--- Day3.py
+++ Day3.py
@@ -8,3 +8,15 @@ import keyword
 # Printing all keywords at once using "Kwlist()"
 print("The list of keyword is : ")
 print(keyword.kwlist)
+
+#Category and keywords in python 
+
+# Values keywords --- True, false , None 
+#operator keyword s- and, or, not, in, is
+# Control Flow Keywords	---------if, else, elif, for, while, break, continue, pass, try, except, finally, raise, assert
+# Function and Class	----------def, return, lambda, yield, class
+# Context Management	-----------------with, as
+# Import and Module	-------------import, from, as
+#  Scope and Namespace-------------------	global, nonlocal
+# Async Programming----------------	async, await
+
