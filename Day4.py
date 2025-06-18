64cad10213da4c5da971a20291b75de3313c871a
anjaliofficial <bistaanjali1415@gmail.com>
2025-06-18 15:21:07 +0545

    Numeric data types in python

---
+++ Day4.py
@@ -0,0 +1,17 @@
+#Data Types in Python 
+# Numeric - int, float, complex
+# Sequence Type - string, list, tuple
+# Mapping Type - dict
+# Boolean - bool
+# Set Type - set, frozenset
+# Binary Types - bytes, bytearray, memoryview
+
+#Example of Numeric data types in python 
+a = 5 # Int 
+print(type(a))
+
+b = 5.0 #Float
+print(type(b))
+
+c = 2+4j   # complex
+print(type(c))
