87e6be163013fdedae0c877b582d300ea50cfb5e
anjaliofficial <bistaanjali1415@gmail.com>
2025-06-19 12:04:22 +0545

    If -elif example

--- Day5.py
+++ Day5.py
@@ -171,3 +171,19 @@ if num > 5:
 
 # The if-elif statement is shortcut of if..else chain. While using if-elif statement at the end 
 # else block is added which is performed if none of the above if-elif statement is true.
+
+# if-elif statement example
+ 
+letter = "A"
+ 
+if letter == "B":
+   print("letter is B")
+ 
+elif letter == "C":
+   print("letter is C")
+ 
+elif letter == "A":
+   print("letter is A")
+ 
+else:
+   print("letter isn't A, B or C")
