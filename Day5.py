d712c8979125b4d9690b61abd402a213e2b75a45
anjaliofficial <bistaanjali1415@gmail.com>
2025-06-19 11:34:00 +0545

    Theory

--- Day5.py
+++ Day5.py
@@ -1,2 +1,6 @@
 #Type conversion in python 
 
+#two types of type conversion in python 
+# Implicit type conversion :- this happens automatically without the programmer writing any specific code for it 
+#  
+#Explicit type conversion :- The  reqiures the programmar to use Specific functions or syntax to convert one type to another
