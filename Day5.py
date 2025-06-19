083cc0935559ef1f17472518330725080771c878
anjaliofficial <bistaanjali1415@gmail.com>
2025-06-19 12:01:18 +0545

    IF, else, if

--- Day5.py
+++ Day5.py
@@ -128,4 +128,27 @@ if x == 4:
     print("Yes")
 else: 
     print("No")
-    
+
+
+
+#Example 2: Example 2: You can also chain if..else statement with more than one condition. 
+
+# if..else chain statement
+letter = "A"
+ 
+if letter == "B":
+  print("letter is B")
+   
+else:
+     
+  if letter == "C":
+    print("letter is C")
+     
+  else:
+       
+    if letter == "A":
+      print("letter is A")
+       
+    else:
+      print("letter isn't A, B and C")
+
