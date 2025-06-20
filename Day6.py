355c7f34b471efbc0e9465615b6c43c71eb4c908
anjaliofficial <bistaanjali1415@gmail.com>
2025-06-20 08:41:30 +0545

    While loop in python

--- Day6.py
+++ Day6.py
@@ -1,1 +1,11 @@
 #Day 6 of learning python 
+
+
+#WWhile looops in Python 
+
+#Hoe the conditions work 
+# The condition_test is a Boolean expression that determines whether the loop continues to execute. As long as this condition remains true, the statements inside the loop will keep running. Once the condition becomes false, the loop will terminate,
+# and the program will move on to the next set of instructions outside the loop.
+
+
+i
