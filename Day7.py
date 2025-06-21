4653bf4f05363af12737b7f8b73d7ef6227f2fba
anjaliofficial <bistaanjali1415@gmail.com>
2025-06-21 22:44:25 +0545

    Break example

--- Day7.py
+++ Day7.py
@@ -6,3 +6,9 @@
 # Loops in Python can be controlled using jump statements like break. These statements allow you to change the flow of execution by exiting loops prematurely or skipping iterations.
 
 # The break statement specifically is used to terminate the loop when a certain condition is met, allowing the program to continue executing the code that follows the loop.
+
+n = 15
+for x in range(2, n+1):
+    if n % x == 0:
+        print(x)
+        break
