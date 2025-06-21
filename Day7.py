28cab080e88958e623e73bbe8573739d5b64c97f
anjaliofficial <bistaanjali1415@gmail.com>
2025-06-21 22:41:09 +0545

    Theory

--- Day7.py
+++ Day7.py
@@ -2,3 +2,7 @@
 
 
 #Break  in python 
+
+# Loops in Python can be controlled using jump statements like break. These statements allow you to change the flow of execution by exiting loops prematurely or skipping iterations.
+
+# The break statement specifically is used to terminate the loop when a certain condition is met, allowing the program to continue executing the code that follows the loop.
