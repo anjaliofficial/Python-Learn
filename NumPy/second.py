57308cbd7fcea223541c328f9f3f969d142883ad
anjaliofficial <bistaanjali1415@gmail.com>
2025-07-06 17:08:56 +0545

    shape array with 4 rows , 2 columns

--- NumPy/second.py
+++ NumPy/second.py
@@ -112,3 +112,10 @@ print("Original array : \n", array1)
 array2 = geek.arange(8).reshape(2, 4)
 print("\narray reshaped with 2 rows and 4 columns : \n",
       array2)
+
+
+# shape array with 4 rows and 2 columns
+array3 = geek.arange(8).reshape(4, 2)
+print("\narray reshaped with 4 rows and 2 columns : \n",
+      array3)
+ 
