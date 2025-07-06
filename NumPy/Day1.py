ae5eb409537b2bb63a8eaa621808be38b1f1c8db
anjaliofficial <bistaanjali1415@gmail.com>
2025-07-06 16:43:30 +0545

    Example of ndarray

--- NumPy/Day1.py
+++ NumPy/Day1.py
@@ -61,6 +61,16 @@ print(b.dtype)
 #Return 
 # ndarray of zeros having given shape, order and datatype.
 
-
-
-
+# Python Program illustrating
+# numpy.zeros method
+ 
+import numpy as geek
+ 
+b = geek.zeros(2, dtype = int)
+print("Matrix b : \n", b)
+ 
+a = geek.zeros([2, 2], dtype = int)
+print("\nMatrix a : \n", a)
+ 
+c = geek.zeros([3, 3])
+print("\nMatrix c : \n", c)
