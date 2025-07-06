fd3e15a0736862f62fd9492fadda43a5baa7663b
anjaliofficial <bistaanjali1415@gmail.com>
2025-07-06 17:04:57 +0545

    Restep set tto true

--- NumPy/second.py
+++ NumPy/second.py
@@ -63,3 +63,14 @@ print("A\n", geek.arange(4, 20, 3), "\n")
 
 # -> ndarray
 # -> step : [float, optional], if restep = True
+
+# Python Programming illustrating
+# numpy.linspace method
+ 
+import numpy as geek
+
+#restep set to True 
+print("B\n", geek.linspace(2.0, 3.0, num=5, retstep=True), "\n")
+# To evaluate sin() in long range
+x = geek.linspace(0, 2, 10)
+print("A\n", geek.sin(x))
