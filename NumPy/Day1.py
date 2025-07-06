46f6202a5ab24e8c70a32bcaaa9c32447d2eb779
anjaliofficial <bistaanjali1415@gmail.com>
2025-07-06 16:33:19 +0545

    Ndim

--- NumPy/Day1.py
+++ NumPy/Day1.py
@@ -7,3 +7,20 @@ print()
 arr = np.array(a)
 print(arr)
 print(type(arr))
+
+
+
+#numpy.ndarray.ndim() function return the number of dimensions of an array.
+#The .ndim attribute (not function) returns the number of dimensions (also called axes or rank) of a NumPy array.
+
+
+
+a = np.array([1,2,3,4])
+b = np.array([[1,2,3,4], [5,6,7,8,]])
+c = np.array([[1,2,3,4], [5,6,7,8,], [9,10, 11, 12]])
+
+print(a.ndim)
+print(b.ndim)
+print(c.ndim)
+
+#return which types of array is 1d or 2d \
