2bd3948ab774755e1a30f0040a210b300a7dd2d8
anjaliofficial <bistaanjali1415@gmail.com>
2025-07-06 16:57:44 +0545

    Rarange

--- NumPy/second.py
+++ NumPy/second.py
@@ -17,3 +17,17 @@ import numpy as geek
 # output array
 out_arr = geek.random.random(3)
 print ("Output 1D Array filled with random floats : ", out_arr)
+
+
+# --------------------------------------
+# The arange([start,] stop[, step,][, dtype]) : Returns an array with evenly spaced elements as per the interval. The interval mentioned is half-opened i.e. [Start, Stop) 
+# Parameters : 
+
+# start : [optional] start of interval range. By default start = 0
+# stop  : end of interval range
+# step  : [optional] step size of interval. By default step size = 1,  
+# For any output out, this is the distance between two adjacent values, out[i+1] - out[i]. 
+# dtype : type of output array
+
+# Array of evenly spaced values.
+# Length of array being generated  = Ceil((Stop - Start) / Step) 
